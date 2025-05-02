e5d6abfe44086e7822807035d5ab7c1c2e44127a
Kevin Nunes <kevin.nunes@al.infnet.edu.br>
2025-05-01 21:15:05 -0300

    tabuada de 1 a 10

---
+++ 02-funcoes-e-calculos/Tabuada.py
@@ -0,0 +1,5 @@
+for numero in range(1, 11):
+    print(f"\nTabuada do {numero}:")  
+    for multiplicador in range(1, 11):
+        resultado = numero * multiplicador
+        print(f"{numero} x {multiplicador} = {resultado}")
